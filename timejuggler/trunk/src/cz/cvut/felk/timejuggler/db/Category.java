9765e312132f42b69e086d1e038c68831fdc9bc2
Vity <Vity@40407085-c429-0410-a8d0-eb1a82597f6e>
2007-08-02 11:57:56 +0000

    podpora rozhrani Comparable, implementace equals a hashcode

--- timejuggler/trunk/src/cz/cvut/felk/timejuggler/db/Category.java
+++ timejuggler/trunk/src/cz/cvut/felk/timejuggler/db/Category.java
@@ -1,83 +1,102 @@
 package cz.cvut.felk.timejuggler.db;
 
 import java.util.logging.Logger;
+
 /**
  * @author Jan Struz
  * @version 0.1
  * @created 12-V-2007 23:40:48
- * 
- * Reprezentuje kategorii eventu nebo tasku..
- * Hotovo
+ * <p/>
+ * Reprezentuje kategorii eventu nebo tasku.. Hotovo
  */
-public class Category extends DbElement{
-	private final static Logger logger = Logger.getLogger(Category.class.getName());
-	
-	private String name;
-	
-	private int componentId = -1;
-	
-	public Category(){
+public class Category extends DbElement implements Comparable {
+    private final static Logger logger = Logger.getLogger(Category.class.getName());
 
-	}
-	
-	public Category(String name){
-		this.name = name;
-	}
-	
+    private String name;
+
+    private int componentId = -1;
+
+    public Category() {
+
+    }
+
+    public Category(String name) {
+        this.name = name;
+    }
+
     public void store() {
 
     }
-	 
-	 /**
+
+    /**
      * Method saveOrUpdate
      * @param template
      */
-	public void saveOrUpdate(TimeJugglerJDBCTemplate template) {
-		if (getId() > 0) {
-			logger.info("Database - Update: Category[" + getId() + "]:" + name + "...");
-			Object params[] = { name, componentId, getId() };
-			String updateQuery = "UPDATE Category SET name=?,calComponentID=? WHERE categoryID = ? ";
-			template.executeUpdate(updateQuery, params);
-		}else{
-			logger.info("Database - Insert: Category[]:" + name + "...");
-	        Object params[] = { name, componentId  };
-	        String insertQuery = "INSERT INTO Category (name,calComponentID) VALUES (?,?) ";
-	        template.executeUpdate(insertQuery, params);
-	        setId(template.getGeneratedId());
-		}
-	}
-	 /**
+    public void saveOrUpdate(TimeJugglerJDBCTemplate template) {
+        if (getId() > 0) {
+            logger.info("Database - Update: Category[" + getId() + "]:" + name + "...");
+            Object params[] = {name, componentId, getId()};
+            String updateQuery = "UPDATE Category SET name=?,calComponentID=? WHERE categoryID = ? ";
+            template.executeUpdate(updateQuery, params);
+        } else {
+            logger.info("Database - Insert: Category[]:" + name + "...");
+            Object params[] = {name, componentId};
+            String insertQuery = "INSERT INTO Category (name,calComponentID) VALUES (?,?) ";
+            template.executeUpdate(insertQuery, params);
+            setId(template.getGeneratedId());
+        }
+    }
+
+    /**
      * Method delete
      * @param template
      */
-	public void delete(TimeJugglerJDBCTemplate template) {
-		if (getId() > 0) {
-			Object params[] = {	getId() };		
-			String deleteQuery = "DELETE FROM Category WHERE categoryID = ? ";
-			template.executeUpdate(deleteQuery, params);
-			setId(-1);
-		}
-	}
+    public void delete(TimeJugglerJDBCTemplate template) {
+        if (getId() > 0) {
+            Object params[] = {getId()};
+            String deleteQuery = "DELETE FROM Category WHERE categoryID = ? ";
+            template.executeUpdate(deleteQuery, params);
+            setId(-1);
+        }
+    }
 
-	public String getName(){
-		return name;
-	}
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
 
-	/**
-	 * 
-	 * @param newVal
-	 */
-	public void setName(String newVal){
-		name = newVal;
-	}
+        Category category = (Category) o;
+        return componentId == category.componentId && name.equals(category.name);
+    }
 
-	
-	public void setComponentId(int componentId) {
-		this.componentId = componentId; 
-	}
+    public int hashCode() {
+        int result;
+        result = name.hashCode();
+        result = 31 * result + componentId;
+        return result;
+    }
 
-	public int getComponentId() {
-		return (this.componentId); 
+    public int compareTo(Object o) {
+        return getName().compareTo(((Category) o).getName());
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param newVal
+     */
+    public void setName(String newVal) {
+        name = newVal;
+    }
+
+
+    public void setComponentId(int componentId) {
+        this.componentId = componentId;
+    }
+
+    public int getComponentId() {
+        return (this.componentId);
 	}
 
 }
