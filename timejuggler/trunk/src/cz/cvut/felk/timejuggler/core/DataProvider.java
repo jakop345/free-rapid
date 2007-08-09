33a8479502569c56ec5ef4bb189335f4fc0a5f11
Vity <Vity@40407085-c429-0410-a8d0-eb1a82597f6e>
2007-08-09 00:27:52 +0000

    pouziti DBDatastoru

--- timejuggler/trunk/src/cz/cvut/felk/timejuggler/core/DataProvider.java
+++ timejuggler/trunk/src/cz/cvut/felk/timejuggler/core/DataProvider.java
@@ -4,20 +4,17 @@
 package cz.cvut.felk.timejuggler.core;
 
 import com.jgoodies.binding.list.ArrayListModel;
-import cz.cvut.felk.timejuggler.db.entity.DbElement;
-import cz.cvut.felk.timejuggler.db.entity.Category;
-import cz.cvut.felk.timejuggler.db.entity.VCalendar;
+import cz.cvut.felk.timejuggler.db.DatabaseException;
+import cz.cvut.felk.timejuggler.db.DbDataStore;
 import cz.cvut.felk.timejuggler.db.TimeJugglerJDBCTemplate;
+import cz.cvut.felk.timejuggler.db.entity.Category;
+import cz.cvut.felk.timejuggler.db.entity.DbElement;
+import cz.cvut.felk.timejuggler.db.entity.VCalendar;
 import org.izvin.client.desktop.ui.util.UIBeanEnhancer;
 
 import java.awt.*;
 import java.util.Arrays;
 import java.util.List;
-import java.util.ArrayList;
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import cz.cvut.felk.timejuggler.db.DatabaseException;
 
 
 /**
@@ -26,14 +23,16 @@ import cz.cvut.felk.timejuggler.db.DatabaseException;
 public class DataProvider {
 
     private static DataProvider ourInstance = new DataProvider();
-    List<Category> categories;
+    private List<Category> categories;
     private ArrayListModel<VCalendar> calendars;
+    private DbDataStore dbStore;
 
     public static DataProvider getInstance() {
         return ourInstance;
     }
 
     private DataProvider() {
+        dbStore = new DbDataStore();
         calendars = new ArrayListModel<VCalendar>();
         categories = Arrays.asList(new Category("Birthday", Color.YELLOW), new Category("Annivesary", Color.BLUE), new Category("Annivesary", Color.BLUE));
 
@@ -56,39 +55,21 @@ public class DataProvider {
     }
 
     public synchronized ArrayListModel<VCalendar> getCalendars() {
-        String sql = "SELECT * FROM VCalendar";
-        TimeJugglerJDBCTemplate<ArrayListModel<VCalendar>> template = new TimeJugglerJDBCTemplate<ArrayListModel<VCalendar>>() {
-            protected void handleRow(ResultSet rs) throws SQLException {
-            	if (items == null) items = new ArrayListModel<VCalendar>();
-                VCalendar cal = new VCalendar();
-                cal.setId(Integer.valueOf(rs.getInt("vCalendarID")).intValue());
-                cal.setProductId(rs.getString("prodid"));
-                cal.setCalendarScale(rs.getString("calscale"));
-                cal.setMethod(rs.getString("method"));
-                cal.setVersion(rs.getString("version"));
-                cal.setName(rs.getString("name"));
-                items.add(cal);
-            }
-        };
-        template.executeQuery(sql, null);
-        calendars = template.getItems();
-
         if (calendars == null) {
-            //fake data
-            List<VCalendar> normalFromDBList = Arrays.asList(new VCalendar("VityCalendar"), new VCalendar("JohnnyCalendar"), new VCalendar("SvatkyCalendar"));
-            calendars = enhanceToBeans(normalFromDBList);
+            //List<VCalendar> normalFromDBList = Arrays.asList(new VCalendar("VityCalendar"), new VCalendar("JohnnyCalendar"), new VCalendar("SvatkyCalendar"));
+            calendars = enhanceToBeans(dbStore.getCalendars());
         }
         return calendars;
     }
 
     /**
      * Method saveOrUpdate
-     *
+     * <p/>
      * Ulozi entitu do databaze
      */
     public <C extends DbElement> void saveOrUpdate(C entity) throws DatabaseException {
-    	TimeJugglerJDBCTemplate template = new TimeJugglerJDBCTemplate();
-    	entity.saveOrUpdate(template);
-    	template.commit();
+        TimeJugglerJDBCTemplate template = new TimeJugglerJDBCTemplate();
+        entity.saveOrUpdate(template);
+        template.commit();
     }
 }
