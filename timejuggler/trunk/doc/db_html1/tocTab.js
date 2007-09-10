7b97f109aed1804ae336fc3ac610705bd30ac3c6
Johnny <Johnny@40407085-c429-0410-a8d0-eb1a82597f6e>
2007-09-10 14:31:46 +0000

    ukazka dokumentace i s uml k package DB

---
+++ timejuggler/trunk/doc/db_html1/tocTab.js
@@ -0,0 +1,5 @@
+var tocTab = new Array();
+tocTab[0] = new Array ("0", "db", "EARoot/contents.htm", "");
+tocTab[1] = new Array ("1", "entity", "EARoot/EA1/contents.htm", "");
+tocTab[2] = new Array ("1.1", "interfaces", "EARoot/EA1/EA1/contents.htm", "");
+var nCols = 20;
