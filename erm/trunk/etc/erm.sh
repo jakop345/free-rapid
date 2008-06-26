5d9bc591fa34d456420c770348c6db11de5b5e19
wordrider <wordrider@40407085-c429-0410-a8d0-eb1a82597f6e>
2008-06-26 22:12:57 +0000

    etc folder

--- erm/trunk/etc/erm.sh
+++ erm/trunk/etc/erm.sh
@@ -1,13 +1,7 @@
 #!/bin/sh
 # How to configure ER modeller installed locally in your computer.
-#			Careful! ER modeller does not work on my system (Debian) until I installed
-#			JRE 1.5.0_08  - build 08 is important just for Debian 
-#			it works properly on previous version on Solaris 9 
-#			I did not tested on another Linux distributions - your notices are welcome
-# 		The rest of variables have relative value to ER_PATH, hence there is no reason to change 
-# 		their values.
 
-# The script bellow shows how I am runnig ER modeller on my Linux (Debian)
+# The script bellow shows how I am running ER modeller on my Linux (Kubuntu)
 
 #How do I register Java binaries globally on Linux?
 #You must place the correct path to the installed binaries in your /etc/bashrc configuration file.
@@ -15,7 +9,7 @@
 #PATH=/jdk1.6.0/bin:/jdk1.6.0/jre/bin:$PATH 
 
 #If you need to set a path to JRE uncomment and update these lines:
-# export ER_JRE_PATH=/opt/jre1.5.0_08
+# export ER_JRE_PATH=/opt/jre1.6.0_08
 # $ER_JRE_PATH/bin/java -jar erm.jar "$@"
 
 #otherwise...
