99857f751c909c140837e7bbff9c81bce52c871a
ATom <ATom@40407085-c429-0410-a8d0-eb1a82597f6e>
2008-09-16 14:00:16 +0000

    INIT - RapidSharePremium plugin (rc verze)

---
+++ freerapid-plugins/trunk/src/rapidshare_premium/cz/vity/freerapid/plugins/services/rapidshare_premium/BadLoginException.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cz.vity.freerapid.plugins.services.rapidshare_premium;
+
+import cz.vity.freerapid.plugins.exceptions.ErrorDuringDownloadingException;
+
+/**
+ * @author Tomáš Procházka &lt;<a href="mailto:tomas.prochazka@atomsoft.cz">tomas.prochazka@atomsoft.cz</a>&gt;
+ * @version $Revision$ ($Date$)
+ */
+public class BadLoginException extends ErrorDuringDownloadingException {
+
+    /** Constructor */
+    public BadLoginException() {
+    }
+
+}
+
