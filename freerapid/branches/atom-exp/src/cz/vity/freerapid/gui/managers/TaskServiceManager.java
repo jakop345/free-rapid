2d9284377adbaa1cfe86842de433b899864125fa
ATom <ATom@40407085-c429-0410-a8d0-eb1a82597f6e>
2012-11-01 07:11:05 +0000

    * merge 4147 - 4187

--- freerapid/branches/atom-exp/src/cz/vity/freerapid/gui/managers/TaskServiceManager.java
+++ freerapid/branches/atom-exp/src/cz/vity/freerapid/gui/managers/TaskServiceManager.java
@@ -1,5 +1,7 @@
 package cz.vity.freerapid.gui.managers;
 
+import cz.vity.freerapid.core.AppPrefs;
+import cz.vity.freerapid.core.UserProp;
 import org.jdesktop.application.ApplicationContext;
 import org.jdesktop.application.Task;
 import org.jdesktop.application.TaskService;
@@ -14,21 +16,24 @@ public class TaskServiceManager {
     private final static Logger logger = Logger.getLogger(TaskServiceManager.class.getName());
 
     public static final String DOWNLOAD_SERVICE = "downloadService";
+    public static final String RUN_CHECK_SERVICE = "runCheckService";
     public static final String MOVE_FILE_SERVICE = "moveFile";
     public static final String WORK_WITH_FILE_SERVICE = "workWithFile";
     public static final String DATABASE_SERVICE = "databaseService";
 
-    private ApplicationContext context;
+    private final ApplicationContext context;
 
-    public TaskServiceManager(ApplicationContext context) {
+    public TaskServiceManager(final ApplicationContext context) {
         this.context = context;
     }
 
-    public synchronized TaskService getTaskService(String name) {
+    public synchronized TaskService getTaskService(final String name) {
         final TaskService service = context.getTaskService(name);
         if (service == null) {
             if (DOWNLOAD_SERVICE.equals(name)) {
                 return initDownloadTaskService();
+            } else if (RUN_CHECK_SERVICE.equals(name)) {
+                return initRunCheckTaskService();
             } else if (MOVE_FILE_SERVICE.equals(name)) {
                 return initMoveFileTaskService();
             } else if (WORK_WITH_FILE_SERVICE.equals(name)) {
@@ -41,28 +46,35 @@ public class TaskServiceManager {
     }
 
     private TaskService initDownloadTaskService() {
-        return initTaskService(0, Integer.MAX_VALUE, 60L, DOWNLOAD_SERVICE, new SynchronousQueue<Runnable>());
+        return initTaskService(DOWNLOAD_SERVICE, Executors.newCachedThreadPool());
+    }
+
+    private TaskService initRunCheckTaskService() {
+        final int max = AppPrefs.getProperty(UserProp.MAX_SIMULTANEOUS_RUN_CHECK, UserProp.MAX_SIMULTANEOUS_RUN_CHECK_DEFAULT);
+        return initTaskService(RUN_CHECK_SERVICE, newExecutorService(max));
     }
 
     private TaskService initMoveFileTaskService() {
-        return initTaskService(1, 1, 60L, MOVE_FILE_SERVICE, new LinkedBlockingQueue<Runnable>());
+        return initTaskService(MOVE_FILE_SERVICE, newExecutorService(1));
     }
 
     private TaskService initWorkWithFileTaskService() {
-        return initTaskService(1, 1, 60L, WORK_WITH_FILE_SERVICE, new LinkedBlockingQueue<Runnable>());
+        return initTaskService(WORK_WITH_FILE_SERVICE, newExecutorService(1));
     }
 
     private TaskService initWorkWithDatabaseTaskService() {
-        return initTaskService(1, 1, 60L, DATABASE_SERVICE, new LinkedBlockingQueue<Runnable>());
+        return initTaskService(DATABASE_SERVICE, Executors.newSingleThreadExecutor());
     }
 
-    private TaskService initTaskService(int corePoolSize, int maximumPoolSize, long keepAliveTime, String name, BlockingQueue<Runnable> runnables) {
-        final ThreadPoolExecutor threadPool = new ThreadPoolExecutor(
-                corePoolSize,   // corePool size
-                maximumPoolSize,  // maximumPool size
-                keepAliveTime, TimeUnit.SECONDS,  // non-core threads time to live
-                runnables);
-        final TaskService service = new TaskService(name, threadPool);
+    private static ExecutorService newExecutorService(final int numThreads) {
+        final ThreadPoolExecutor executor = new ThreadPoolExecutor(
+                numThreads, numThreads, 60L, TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>());
+        executor.allowCoreThreadTimeOut(true);
+        return executor;
+    }
+
+    private TaskService initTaskService(final String name, final ExecutorService executor) {
+        final TaskService service = new TaskService(name, executor);
         context.addTaskService(service);
         logger.info("Creating pool " + name);
         return service;
@@ -71,4 +83,5 @@ public class TaskServiceManager {
     public void runTask(String taskServiceName, Task task) {
         this.getTaskService(taskServiceName).execute(task);
     }
+
 }
