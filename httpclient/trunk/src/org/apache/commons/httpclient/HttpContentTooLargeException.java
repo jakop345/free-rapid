bfe8dbebab633d18bd63623749423ae25cf15d64
tong2shot <tong2shot@40407085-c429-0410-a8d0-eb1a82597f6e>
2013-02-23 14:55:19 +0000

    Initial httpClient repo

---
+++ httpclient/trunk/src/org/apache/commons/httpclient/HttpContentTooLargeException.java
@@ -0,0 +1,22 @@
+package org.apache.commons.httpclient;
+
+/**
+ * Signals that the response content was larger than anticipated. 
+ * 
+ * @author Ortwin Gl�ck
+ */
+public class HttpContentTooLargeException extends HttpException {
+    private int maxlen;
+
+    public HttpContentTooLargeException(String message, int maxlen) {
+        super(message);
+        this.maxlen = maxlen;
+    }
+    
+    /**
+     * @return the maximum anticipated content length in bytes.
+     */
+    public int getMaxLength() {
+        return maxlen;
+    }
+}
