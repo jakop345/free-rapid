60153fb9771b8aac419ef82f5409c587a5ad82d5
birchie <birchie@40407085-c429-0410-a8d0-eb1a82597f6e>
2014-04-10 22:01:05 +0000

    New Plugin:  DropBox.com       Fixed:  Titulky.com, CzShare -> Sdilej.cz

--- freerapid-plugins/trunk/src/dropbox/cz/vity/freerapid/plugins/services/dropbox/TestApp.java
+++ freerapid-plugins/trunk/src/dropbox/cz/vity/freerapid/plugins/services/dropbox/TestApp.java
@@ -1,36 +1,32 @@
-package cz.vity.freerapid.plugins.services.czshare;
+package cz.vity.freerapid.plugins.services.dropbox;
 
 import cz.vity.freerapid.plugins.dev.PluginDevApplication;
 import cz.vity.freerapid.plugins.webclient.ConnectionSettings;
 import cz.vity.freerapid.plugins.webclient.interfaces.HttpFile;
-import cz.vity.freerapid.utilities.LogUtils;
 import org.jdesktop.application.Application;
 
 import java.net.URL;
-import java.util.logging.Logger;
 
 /**
- * @author Vity
+ * @author birchie
  */
 public class TestApp extends PluginDevApplication {
-    private final static Logger logger = Logger.getLogger(TestApp.class.getName());
-
     @Override
     protected void startup() {
         final HttpFile httpFile = getHttpFile(); //creates new test instance of HttpFile
         try {
             //we set file URL
-            //httpFile.setNewURL(new URL("http://czshare.com/3102571/11-20.rar"));
-            httpFile.setNewURL(new URL("http://sdilej.cz/6046504/Nikita.314.TVRIP.XviD_cz.avi"));
+            httpFile.setNewURL(new URL("https://www.dropbox.com/s/gn3sr42r1gtuiah/The%20Ironwood%20Tree%20%28The%20Spiderwick%20Chronicles%20%234%29%20by%20Holly%20Black%2C%20Tony%20DiTerlizzi.epub"));
             //the way we connect to the internet
             final ConnectionSettings connectionSettings = new ConnectionSettings();// creates default connection
-            //connectionSettings.setProxy("222.124.191.186", 8080);//eg we can use local proxy to sniff HTTP communication
+            //connectionSettings.setProxy("localhost", 8081); //eg we can use local proxy to sniff HTTP communication
             //then we tries to download
-            final CzshareServiceImpl service = new CzshareServiceImpl(); //instance of service - of our plugin
+            final DropBoxServiceImpl service = new DropBoxServiceImpl(); //instance of service - of our plugin
+            //runcheck makes the validation
             testRun(service, httpFile, connectionSettings);//download file with service and its Runner
             //all output goes to the console
         } catch (Exception e) {//catch possible exception
-            LogUtils.processException(logger, e); //writes error output - stack trace to console
+            e.printStackTrace(); //writes error output - stack trace to console
         }
         this.exit();//exit application
     }
