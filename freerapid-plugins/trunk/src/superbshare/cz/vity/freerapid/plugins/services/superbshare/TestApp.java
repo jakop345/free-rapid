42dbb768027739161cd544f6444dc081432a32be
birchie <birchie@40407085-c429-0410-a8d0-eb1a82597f6e>
2014-11-21 21:00:19 +0000

    Fixed:  SuperBShare.com

--- freerapid-plugins/trunk/src/superbshare/cz/vity/freerapid/plugins/services/superbshare/TestApp.java
+++ freerapid-plugins/trunk/src/superbshare/cz/vity/freerapid/plugins/services/superbshare/TestApp.java
@@ -17,7 +17,7 @@ public class TestApp extends PluginDevApplication {
         final HttpFile httpFile = getHttpFile(); //creates new test instance of HttpFile
         try {
             //we set file URL
-            httpFile.setNewURL(new URL("http://www.superbshare.com/file/Rxk"));
+            httpFile.setNewURL(new URL("http://superbshare.com/file/YjYW"));
             //the way we connect to the internet
             final ConnectionSettings connectionSettings = new ConnectionSettings();// creates default connection
             //connectionSettings.setProxy("localhost", 8081); //eg we can use local proxy to sniff HTTP communication
