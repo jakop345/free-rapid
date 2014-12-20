4fc675725a55c6bbd3bbea63cd602d2c85c51e20
tong2shot <tong2shot@40407085-c429-0410-a8d0-eb1a82597f6e>
2014-12-20 10:49:32 +0000

    ExtMatrix: https support. tn.nova.cz : new plugin.

---
+++ freerapid-plugins/trunk/src/nova_tn/cz/vity/freerapid/plugins/services/nova_tn/Crypto.java
@@ -0,0 +1,34 @@
+package cz.vity.freerapid.plugins.services.nova_tn;
+
+import org.apache.commons.codec.binary.Base64;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Arrays;
+
+/**
+ * @author tong2shot
+ */
+class Crypto {
+    private final static byte[] PASSWORD = "EaDUutg4ppGYXwNMFdRJsadenFSnI6gJ".getBytes();
+
+    private SecretKey generateKey(byte[] pwBytes) throws Exception {
+        byte[] _pwBytes = Arrays.copyOf(pwBytes, 16);
+        SecretKey secretKey = new SecretKeySpec(_pwBytes, "AES");
+        Cipher cipher = Cipher.getInstance("AES/ECB/NoPadding");
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+        return new SecretKeySpec(cipher.doFinal(_pwBytes), "AES");
+    }
+
+    public String decrypt(String base64CipherText) throws Exception {
+        byte[] cipherTextBytes = Base64.decodeBase64(base64CipherText);
+        byte[] nonceBytes = Arrays.copyOf(Arrays.copyOf(cipherTextBytes, 8), 16);
+        IvParameterSpec nonce = new IvParameterSpec(nonceBytes);
+        Cipher cipher = Cipher.getInstance("AES/CTR/NoPadding");
+        cipher.init(Cipher.DECRYPT_MODE, generateKey(PASSWORD), nonce);
+        byte[] decrypted = cipher.doFinal(cipherTextBytes, 8, cipherTextBytes.length - 8);
+        return new String(decrypted);
+    }
+}
