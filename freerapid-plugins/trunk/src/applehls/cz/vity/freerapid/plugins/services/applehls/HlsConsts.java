9a5050f7113c4dbfb7a20085f794b64281907759
tong2shot <tong2shot@40407085-c429-0410-a8d0-eb1a82597f6e>
2015-01-28 23:13:06 +0000

    AppleHLS: add resume support.

---
+++ freerapid-plugins/trunk/src/applehls/cz/vity/freerapid/plugins/services/applehls/HlsConsts.java
@@ -0,0 +1,10 @@
+package cz.vity.freerapid.plugins.services.applehls;
+
+/**
+ * @author tong2shot
+ */
+public final class HlsConsts {
+
+    public final static String SEGMENT_LAST_POST = "segmentLastPos";
+    public final static String CURRENT_SEGMENT = "currentSegment";
+}
--- freerapid-plugins/trunk/src/applehls/cz/vity/freerapid/plugins/services/applehls/HlsInputStream.java
+++ freerapid-plugins/trunk/src/applehls/cz/vity/freerapid/plugins/services/applehls/HlsInputStream.java
@@ -1,6 +1,8 @@
 package cz.vity.freerapid.plugins.services.applehls;
 
 
+import cz.vity.freerapid.plugins.webclient.DownloadClient;
+
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -10,42 +12,32 @@ import java.io.InputStream;
 public class HlsInputStream extends InputStream {
 
     private final SegmentRequester requester;
+    private long pos;
     private InputStream currentStream = null;
 
     public HlsInputStream(final SegmentRequester requester) {
         this.requester = requester;
+        Long startPos = (Long) requester.httpFile.getProperties().get(DownloadClient.START_POSITION);
+        this.pos = (startPos == null ? 0 : startPos);
     }
 
     @Override
     public int read() throws IOException {
-        final byte[] b = new byte[1];
-        final int len = read(b, 0, 1);
-        if (len == -1) {
-            return -1;
-        }
-        return b[0] & 0xff;
-    }
-
-    @Override
-    public int read(final byte[] b) throws IOException {
-        return read(b, 0, b.length);
-    }
-
-    @Override
-    public synchronized int read(final byte[] b, final int off, final int len) throws IOException {
         int read = 0;
-        if ((currentStream == null) || ((read = currentStream.read(b, off, len)) == -1)) {
+        if ((currentStream == null) || (read = currentStream.read()) == -1) {
             if (read == -1) {
-                currentStream.close();
+                requester.httpFile.getProperties().put(HlsConsts.SEGMENT_LAST_POST, pos);
+                //currentStream.close(); //org.apache.commons.httpclient.AutoCloseInputStream
             }
-            InputStream stream = requester.nextFragment();
+            InputStream stream = requester.nextSegment();
             if (stream == null) {
                 return -1;
             }
             currentStream = stream;
-            return currentStream.read(b, off, len);
+            pos++;
+            return currentStream.read();
         }
+        pos++;
         return read;
     }
-
 }
