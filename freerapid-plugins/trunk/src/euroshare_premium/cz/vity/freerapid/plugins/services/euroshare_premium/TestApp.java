cc0414f8ab471be1814a1e2d5f7c016c013d8f6a
birchie <birchie@40407085-c429-0410-a8d0-eb1a82597f6e>
2015-01-30 22:48:29 +0000

    Fixed:  EuroShare.eu_Premium

--- freerapid-plugins/trunk/src/euroshare_premium/cz/vity/freerapid/plugins/services/euroshare_premium/TestApp.java
+++ freerapid-plugins/trunk/src/euroshare_premium/cz/vity/freerapid/plugins/services/euroshare_premium/TestApp.java
@@ -17,7 +17,7 @@ public class TestApp extends PluginDevApplication {
         final HttpFile httpFile = getHttpFile(); //creates new test instance of HttpFile
         try {
             //we set file URL
-            httpFile.setNewURL(new URL("http://euroshare.eu/file/15842815/logovq.zip"));
+            httpFile.setNewURL(new URL("http://euroshare.eu/file/4FI5PNq9l8/d-yoprvi.part2.rar"));
             //the way we connect to the internet
             final ConnectionSettings connectionSettings = new ConnectionSettings();// creates default connection
             //connectionSettings.setProxy("localhost", 8081); //eg we can use local proxy to sniff HTTP communication
