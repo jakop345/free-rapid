34f99ba0bb9627a6ea482ad4be39324f5fbe86fd
birchie <birchie@40407085-c429-0410-a8d0-eb1a82597f6e>
2015-02-04 01:31:52 +0000

    Fixed:  FileForever.net, KingFiles.net        New Plugins:  FileDust.net, MediaFree.co, MirrorUpload.net, RockFile.eu, UsersCloud.com, ViduShare.com

---
+++ freerapid-plugins/trunk/src/userscloud/cz/vity/freerapid/plugins/services/userscloud/UsersCloudFileRunner.java
@@ -0,0 +1,59 @@
+package cz.vity.freerapid.plugins.services.userscloud;
+
+import cz.vity.freerapid.plugins.exceptions.ErrorDuringDownloadingException;
+import cz.vity.freerapid.plugins.exceptions.InvalidURLOrServiceProblemException;
+import cz.vity.freerapid.plugins.exceptions.PluginImplementationException;
+import cz.vity.freerapid.plugins.services.xfilesharing.XFileSharingRunner;
+import cz.vity.freerapid.plugins.services.xfilesharing.nameandsize.FileNameHandler;
+import cz.vity.freerapid.plugins.services.xfilesharing.nameandsize.FileSizeHandler;
+import cz.vity.freerapid.plugins.webclient.interfaces.HttpFile;
+import cz.vity.freerapid.plugins.webclient.utils.PlugUtils;
+
+import java.util.List;
+import java.util.regex.Matcher;
+
+/**
+ * Class which contains main code
+ *
+ * @author birchie
+ */
+class UsersCloudFileRunner extends XFileSharingRunner {
+
+    @Override
+    protected void correctURL() throws Exception {
+        final Matcher match = PlugUtils.matcher("userscloud\\.com/([\\w\\d]+)", fileURL);
+        if (!match.find())
+            throw new InvalidURLOrServiceProblemException("File ID missing from URL");
+        fileURL = "http://userscloud.com/" + match.group(1);
+    }
+
+    @Override
+    protected List<FileNameHandler> getFileNameHandlers() {
+        final List<FileNameHandler> fileNameHandlers = super.getFileNameHandlers();
+        fileNameHandlers.add(new FileNameHandler() {
+            @Override
+            public void checkFileName(HttpFile httpFile, String content) throws ErrorDuringDownloadingException {
+                final Matcher match = PlugUtils.matcher("[\\]>]([^\\[\\]<>]+?) - [^\\[\\]<>]+?[\\[<]", content);
+                if (!match.find())
+                    throw  new PluginImplementationException("File name not found");
+                httpFile.setFileName(match.group(1).trim());
+            }
+        });
+        return fileNameHandlers;
+    }
+
+    @Override
+    protected List<FileSizeHandler> getFileSizeHandlers() {
+        final List<FileSizeHandler> fileSizeHandlers = super.getFileSizeHandlers();
+        fileSizeHandlers.add(new FileSizeHandler() {
+            @Override
+            public void checkFileSize(HttpFile httpFile, String content) throws ErrorDuringDownloadingException {
+                final Matcher match = PlugUtils.matcher("[\\]>][^\\[\\]<>]+? - ([^\\[\\]<>]+?)[\\[<]", content);
+                if (!match.find())
+                    throw  new PluginImplementationException("File size not found");
+                httpFile.setFileSize(PlugUtils.getFileSizeFromString(match.group(1).trim()));
+            }
+        });
+        return fileSizeHandlers;
+    }
+}
