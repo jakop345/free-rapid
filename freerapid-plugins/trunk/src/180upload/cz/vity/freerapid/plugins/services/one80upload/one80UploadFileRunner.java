cbe0844e3177852aee417a64edf7a97d678779cc
birchie <birchie@40407085-c429-0410-a8d0-eb1a82597f6e>
2015-02-26 23:08:35 +0000

    Fixed:  180Upload.com

--- freerapid-plugins/trunk/src/180upload/cz/vity/freerapid/plugins/services/one80upload/one80UploadFileRunner.java
+++ freerapid-plugins/trunk/src/180upload/cz/vity/freerapid/plugins/services/one80upload/one80UploadFileRunner.java
@@ -1,8 +1,12 @@
 package cz.vity.freerapid.plugins.services.one80upload;
 
+import cz.vity.freerapid.plugins.exceptions.ErrorDuringDownloadingException;
+import cz.vity.freerapid.plugins.exceptions.URLNotAvailableAnymoreException;
 import cz.vity.freerapid.plugins.services.xfilesharing.XFileSharingRunner;
+import cz.vity.freerapid.plugins.webclient.utils.PlugUtils;
 
 import java.util.List;
+import java.util.regex.Matcher;
 
 /**
  * Class which contains main code
@@ -29,5 +33,15 @@ class one80UploadFileRunner extends XFileSharingRunner {
         final List<String> downloadLinkRegexes = super.getDownloadLinkRegexes();
         downloadLinkRegexes.add(0, "var file_link\\s*?=\\s*?['\"](.+?)['\"]");
         return downloadLinkRegexes;
+    }
+
+    @Override    protected void checkFileProblems() throws ErrorDuringDownloadingException {
+        final String content = getContentAsString();
+        try {
+            super.checkFileProblems();
+        } catch (URLNotAvailableAnymoreException e) {
+            final Matcher match = PlugUtils.matcher("style=\"color:\\s*?transparent;\"[^>]*?>\\s*?File Not Found", content);
+            if (!match.find())  throw e;
+        }
     }
 }
