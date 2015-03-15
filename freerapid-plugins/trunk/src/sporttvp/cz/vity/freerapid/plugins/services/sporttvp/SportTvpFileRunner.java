c5096584c8b45e3bcfb5828d2e3bff64be66f3ff
tong2shot <tong2shot@40407085-c429-0410-a8d0-eb1a82597f6e>
2015-03-15 10:49:57 +0000

    sport.tvp.pl : add quality selection support.

--- freerapid-plugins/trunk/src/sporttvp/cz/vity/freerapid/plugins/services/sporttvp/SportTvpFileRunner.java
+++ freerapid-plugins/trunk/src/sporttvp/cz/vity/freerapid/plugins/services/sporttvp/SportTvpFileRunner.java
@@ -6,10 +6,17 @@ import cz.vity.freerapid.plugins.exceptions.ServiceConnectionProblemException;
 import cz.vity.freerapid.plugins.exceptions.URLNotAvailableAnymoreException;
 import cz.vity.freerapid.plugins.webclient.AbstractRunner;
 import cz.vity.freerapid.plugins.webclient.FileState;
+import cz.vity.freerapid.plugins.webclient.utils.JsonMapper;
 import cz.vity.freerapid.plugins.webclient.utils.PlugUtils;
+import cz.vity.freerapid.utilities.LogUtils;
 import org.apache.commons.httpclient.HttpMethod;
 import org.apache.commons.httpclient.methods.GetMethod;
+import org.codehaus.jackson.JsonNode;
 
+import java.io.IOException;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.logging.Logger;
 import java.util.regex.Matcher;
 
@@ -17,28 +24,36 @@ import java.util.regex.Matcher;
  * Class which contains main code
  *
  * @author birchie
+ * @author tong2shot (quality selection)
  */
 class SportTvpFileRunner extends AbstractRunner {
     private final static Logger logger = Logger.getLogger(SportTvpFileRunner.class.getName());
 
+    private SettingsConfig config;
+
+    private void setConfig() throws Exception {
+        SportTvpServiceImpl service = (SportTvpServiceImpl) getPluginService();
+        config = service.getConfig();
+    }
+
     @Override
     public void runCheck() throws Exception { //this method validates file
         super.runCheck();
-        final GetMethod getMethod = getGetMethod(fileURL);//make first request
+        final GetMethod getMethod = getGetMethod(getPlaylistUrl(getVideoId()));//make first request
         if (makeRedirectedRequest(getMethod)) {
             checkProblems();
-            checkNameAndSize(getContentAsString());//ok let's extract file name and size from the page
+            checkNameAndSize(getRootNode(getContentAsString()));//ok let's extract file name and size from the page
         } else {
             checkProblems();
             throw new ServiceConnectionProblemException();
         }
     }
 
-    private void checkNameAndSize(String content) throws ErrorDuringDownloadingException {
-        final Matcher match = PlugUtils.matcher("<h2[^<>]*?>(.+?)<", content);
-        if (!match.find())
+    private void checkNameAndSize(JsonNode rootNode) throws ErrorDuringDownloadingException {
+        String title = rootNode.findPath("title").getTextValue();
+        if (title == null)
             throw new PluginImplementationException("File name not found");
-        httpFile.setFileName(match.group(1));
+        httpFile.setFileName(title);
         httpFile.setFileState(FileState.CHECKED_AND_EXISTING);
     }
 
@@ -46,25 +61,18 @@ class SportTvpFileRunner extends AbstractRunner {
     public void run() throws Exception {
         super.run();
         logger.info("Starting download in TASK " + fileURL);
-        final GetMethod method = getGetMethod(fileURL); //create GET request
+        final GetMethod method = getGetMethod(getPlaylistUrl(getVideoId())); //create GET request
         if (makeRedirectedRequest(method)) { //we make the main request
-            final String contentAsString = getContentAsString();//check for response
             checkProblems();//check problems
-            checkNameAndSize(contentAsString);//extract file name and size from the page
+            JsonNode rootNode = getRootNode(getContentAsString());
+            checkNameAndSize(rootNode);//extract file name and size from the page
 
-            final HttpMethod aMethod = getMethodBuilder().setReferer(fileURL)
-                    .setActionFromIFrameSrcWhereTagContains("player").toGetMethod();
-            if (!makeRedirectedRequest(aMethod)) {
-                checkProblems();
-                throw new ServiceConnectionProblemException();
-            }
-            checkProblems();
-
-            final Matcher match = PlugUtils.matcher("src\\s*?:\\s*?'(.+?)',\\s*?type\\s*?:\\s*?'video", getContentAsString());
-            if (!match.find())
-                throw new PluginImplementationException("Download link not found");
-            final HttpMethod httpMethod = getGetMethod(match.group(1));
-            httpFile.setFileName(httpFile.getFileName() + match.group(1).substring(match.group(1).lastIndexOf(".")));
+            setConfig();
+            SportTvpVideo sportTvpVideo = getSelectedVideo(rootNode);
+            logger.info("Config settings: " + config);
+            logger.info("Downloading video: " + sportTvpVideo);
+            final HttpMethod httpMethod = getGetMethod(sportTvpVideo.url);
+            httpFile.setFileName(httpFile.getFileName() + sportTvpVideo.url.substring(sportTvpVideo.url.lastIndexOf(".")));
             if (!tryDownloadAndSaveFile(httpMethod)) {
                 checkProblems();//if downloading failed
                 throw new ServiceConnectionProblemException("Error starting download");//some unknown problem
@@ -77,8 +85,94 @@ class SportTvpFileRunner extends AbstractRunner {
 
     private void checkProblems() throws ErrorDuringDownloadingException {
         final String contentAsString = getContentAsString();
-        if (contentAsString.contains("File Not Found") || contentAsString.contains("HTTP.404")) {
+        if (contentAsString.contains("\"status\":\"NOT_FOUND\"")) {
             throw new URLNotAvailableAnymoreException("File not found"); //let to know user in FRD
+        }
+    }
+
+    private String getVideoId() throws PluginImplementationException {
+        Matcher matcher = PlugUtils.matcher("tvp\\.pl/(\\d+)/", fileURL);
+        if (!matcher.find()) {
+            throw new PluginImplementationException("Video ID not found");
+        }
+        return matcher.group(1);
+    }
+
+    private String getPlaylistUrl(String videoId) {
+        return "http://www.tvp.pl/shared/cdn/tokenizer_v2.php?object_id=" + videoId;
+    }
+
+    private JsonNode getRootNode(String content) throws PluginImplementationException {
+        JsonNode rootNode;
+        try {
+            rootNode = new JsonMapper().getObjectMapper().readTree(content);
+        } catch (IOException e) {
+            throw new PluginImplementationException("Error parsing JSON content", e);
+        }
+        return rootNode;
+    }
+
+    private SportTvpVideo getSelectedVideo(JsonNode rootNode) throws Exception {
+        List<SportTvpVideo> sportTvpVideos = new LinkedList<SportTvpVideo>();
+        JsonNode formatsNode = rootNode.findPath("formats");
+        if (!formatsNode.isMissingNode()) {
+            int totalBitrate;
+            String url;
+            String mimeType;
+            for (JsonNode formatsItem : formatsNode) {
+                try {
+                    mimeType = formatsItem.findPath("mimeType").getTextValue();
+                    if (mimeType.startsWith("video")) {
+                        totalBitrate = formatsItem.findPath("totalBitrate").getIntValue() / 1000; //kbps
+                        url = formatsItem.findPath("url").getTextValue();
+                        if ((totalBitrate != 0) && (url != null)) {
+                            SportTvpVideo sportTvpVideo = new SportTvpVideo(totalBitrate, url);
+                            logger.info("Found video: " + sportTvpVideo);
+                            sportTvpVideos.add(sportTvpVideo);
+                        }
+                    }
+                } catch (Exception e) {
+                    LogUtils.processException(logger, e);
+                }
+            }
+        }
+        if (sportTvpVideos.isEmpty()) {
+            throw new PluginImplementationException("No available videos");
+        }
+        return Collections.min(sportTvpVideos);
+    }
+
+    private class SportTvpVideo implements Comparable<SportTvpVideo> {
+        private final static int LOWER_QUALITY_PENALTY = 10;
+        private final int bitrate; //kbps
+        private final String url;
+        private final int weight;
+
+        private SportTvpVideo(int bitrate, String url) {
+            this.bitrate = bitrate;
+            this.url = url;
+            this.weight = calcWeight();
+        }
+
+        private int calcWeight() {
+            VideoQuality configQuality = config.getVideoQuality();
+            int deltaBitrate = bitrate - configQuality.getBitrate();
+            return (deltaBitrate < 0 ? Math.abs(deltaBitrate) + LOWER_QUALITY_PENALTY : deltaBitrate);
+        }
+
+        @SuppressWarnings("NullableProblems")
+        @Override
+        public int compareTo(SportTvpVideo that) {
+            return Integer.valueOf(this.weight).compareTo(that.weight);
+        }
+
+        @Override
+        public String toString() {
+            return "SportTvpVideo{" +
+                    "bitrate=" + bitrate + " kbps" +
+                    ", url='" + url + '\'' +
+                    ", weight=" + weight +
+                    '}';
         }
     }
 
