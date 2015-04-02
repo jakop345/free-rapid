0858938587c3d2275e0c6c6a5f18398b1c6475fd
birchie <birchie@40407085-c429-0410-a8d0-eb1a82597f6e>
2015-04-02 23:19:45 +0000

    Fixed:  Instagram.com            New Plugin:  ElsFile.com

---
+++ freerapid-plugins/trunk/src/elsfile/cz/vity/freerapid/plugins/services/elsfile/ElsFileFileRunner.java
@@ -0,0 +1,28 @@
+package cz.vity.freerapid.plugins.services.elsfile;
+
+import cz.vity.freerapid.plugins.services.xfilesharing.XFileSharingRunner;
+import cz.vity.freerapid.plugins.services.xfilesharing.nameandsize.FileSizeHandler;
+import cz.vity.freerapid.plugins.services.xfilesharing.nameandsize.FileSizeHandlerNoSize;
+import cz.vity.freerapid.plugins.webclient.MethodBuilder;
+
+import java.util.List;
+
+/**
+ * Class which contains main code
+ *
+ * @author birchie
+ */
+class ElsFileFileRunner extends XFileSharingRunner {
+
+    @Override
+    protected List<FileSizeHandler> getFileSizeHandlers() {
+        final List<FileSizeHandler> fileSizeHandlers = super.getFileSizeHandlers();
+        fileSizeHandlers.add(new FileSizeHandlerNoSize());
+        return fileSizeHandlers;
+    }
+
+    @Override
+    protected void stepPassword(final MethodBuilder methodBuilder) throws Exception {
+    }
+
+}
