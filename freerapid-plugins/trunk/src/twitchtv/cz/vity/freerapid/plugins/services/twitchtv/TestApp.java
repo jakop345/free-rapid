2d09e1cdf8f8a9b730ffe4f418591c44ba304bf2
tong2shot <tong2shot@40407085-c429-0410-a8d0-eb1a82597f6e>
2015-05-07 22:59:05 +0000

    AppleHLS: playlist URL as base URL. Twitch.tv v1.2.0: - add support for chapter (/c/). - add support for vod (/v/), AppleHLS. - multiparts using technique similar to ceskatelevize, canalbrasilglobo.

--- freerapid-plugins/trunk/src/twitchtv/cz/vity/freerapid/plugins/services/twitchtv/TestApp.java
+++ freerapid-plugins/trunk/src/twitchtv/cz/vity/freerapid/plugins/services/twitchtv/TestApp.java
@@ -16,13 +16,22 @@ public class TestApp extends PluginDevApplication {
         final HttpFile httpFile = getHttpFile();
         try {
             //httpFile.setNewURL(new URL("http://www.twitch.tv/paradoxinteractive/b/326746473"));
-            httpFile.setNewURL(new URL("http://media-cdn.twitch.tv/store44.media44/archives/2012-7-30/highlight_326746473.flv?title=Oops%21+Penalty%21"));
+            //httpFile.setNewURL(new URL("http://media-cdn.twitch.tv/store44.media44/archives/2012-7-30/highlight_326746473.flv?title=Oops%21+Penalty%21"));
             //httpFile.setNewURL(new URL("http://www.twitch.tv/tsm_chaox/b/328855029"));
             //httpFile.setNewURL(new URL("http://en.twitch.tv/teamsp00ky/b/330898023")); //multiparts
             //httpFile.setNewURL(new URL("http://en.twitch.tv/teamsp00ky/b/384333611"));
+
+            //httpFile.setNewURL(new URL("http://www.twitch.tv/saltthesalty/b/656119014")); //archive multiparts
+            //httpFile.setNewURL(new URL("http://www.twitch.tv/saltthesalty/b/656119014?itempos=2&title=More+GTA+V+%2F%2F+More+peyote+hunting+%3AD-2")); //archive multiparts item
+            //httpFile.setNewURL(new URL("http://www.twitch.tv/bradacus/c/6614432")); //chapter multiparts
+            //httpFile.setNewURL(new URL("http://www.twitch.tv/bradacus/c/6614432?itempos=6&title=Trevor+Visits+A+Bar...-6")); //chapter multiparts item
+
+            httpFile.setNewURL(new URL("http://www.twitch.tv/norddeutscherjunge/v/4484480")); //vod
+            //httpFile.setNewURL(new URL("http://www.twitch.tv/wgleu/v/4404821")); //vod
             final ConnectionSettings connectionSettings = new ConnectionSettings();
             //connectionSettings.setProxy("localhost", 8081); //eg we can use local proxy to sniff HTTP communication
             final TwitchTvServiceImpl service = new TwitchTvServiceImpl();
+            //setUseTempFiles(true);
             testRun(service, httpFile, connectionSettings);
         } catch (Exception e) {
             e.printStackTrace();
