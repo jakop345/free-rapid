2ce498ddac69a4cfce9797bd3e2e83b5da38ca21
tong2shot <tong2shot@40407085-c429-0410-a8d0-eb1a82597f6e>
2015-05-11 11:35:49 +0000

    UpToBox: fix login.

--- freerapid-plugins/trunk/src/uptobox/cz/vity/freerapid/plugins/services/uptobox/UptoBoxFileRunner.java
+++ freerapid-plugins/trunk/src/uptobox/cz/vity/freerapid/plugins/services/uptobox/UptoBoxFileRunner.java
@@ -1,12 +1,11 @@
 package cz.vity.freerapid.plugins.services.uptobox;
 
-import cz.vity.freerapid.plugins.exceptions.ErrorDuringDownloadingException;
-import cz.vity.freerapid.plugins.exceptions.NotRecoverableDownloadException;
-import cz.vity.freerapid.plugins.exceptions.PluginImplementationException;
-import cz.vity.freerapid.plugins.exceptions.URLNotAvailableAnymoreException;
+import cz.vity.freerapid.plugins.exceptions.*;
 import cz.vity.freerapid.plugins.services.xfilesharing.XFileSharingRunner;
 import cz.vity.freerapid.plugins.services.xfilesharing.nameandsize.FileNameHandler;
+import cz.vity.freerapid.plugins.webclient.hoster.PremiumAccount;
 import cz.vity.freerapid.plugins.webclient.utils.PlugUtils;
+import org.apache.commons.httpclient.HttpMethod;
 
 import java.util.List;
 import java.util.regex.Matcher;
@@ -50,7 +49,8 @@ class UptoBoxFileRunner extends XFileSharingRunner {
         if (content.contains("Vous ne pouvez pas t&eacute;l&eacute;charger des fichiers de taille sup&eacute;rieur &agrave")) {
             throw new NotRecoverableDownloadException(PlugUtils.getStringBetween(content, " class=\"err\">", "<br").replace("Vous ne pouvez pas t&eacute;l&eacute;charger des fichiers de taille sup&eacute;rieur &agrave", "You can not download file sizes greater than"));
         }
-        try {super.checkDownloadProblems();
+        try {
+            super.checkDownloadProblems();
         } catch (PluginImplementationException x) {
             if (!x.getMessage().contains("Skipped countdown"))      // ignore error
                 throw new PluginImplementationException(x.getMessage());
@@ -64,7 +64,8 @@ class UptoBoxFileRunner extends XFileSharingRunner {
         return downloadPageMarkers;
     }
 
-    @Override    protected List<String> getDownloadLinkRegexes() {
+    @Override
+    protected List<String> getDownloadLinkRegexes() {
         final List<String> downloadLinkRegexes = super.getDownloadLinkRegexes();
         downloadLinkRegexes.add(0, "product_download_url\\s*?=\\s*?(http.+?" + Pattern.quote(httpFile.getFileName()) + ")[\"']");
         return downloadLinkRegexes;
@@ -77,5 +78,29 @@ class UptoBoxFileRunner extends XFileSharingRunner {
             ret = ret.replaceFirst("http://", "https://");
         }
         return ret;
+    }
+
+    @Override
+    protected void doLogin(final PremiumAccount pa) throws Exception {
+        HttpMethod method = getMethodBuilder()
+                .setReferer(getBaseURL())
+                .setAction("https://login.uptobox.com/")
+                .toGetMethod();
+        if (!makeRedirectedRequest(method)) {
+            throw new ServiceConnectionProblemException();
+        }
+        method = getMethodBuilder()
+                .setReferer("https://login.uptobox.com/")
+                .setAction("https://login.uptobox.com/")
+                .setActionFromFormByName("FL", true)
+                .setParameter("login", pa.getUsername())
+                .setParameter("password", pa.getPassword())
+                .toPostMethod();
+        if (!makeRedirectedRequest(method)) {
+            throw new ServiceConnectionProblemException();
+        }
+        if (getContentAsString().contains("Incorrect Login or Password")) {
+            throw new BadLoginException("Invalid account login information");
+        }
     }
 }
