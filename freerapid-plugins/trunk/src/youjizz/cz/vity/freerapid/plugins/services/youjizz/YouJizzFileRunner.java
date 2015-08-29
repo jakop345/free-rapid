54489825e9544b7c2d18e092c198464e67310c64
birchie <birchie@40407085-c429-0410-a8d0-eb1a82597f6e>
2015-08-29 21:42:33 +0000

    Fixed:  LinkBucks, VideoMega.tv, xHamster.com              New Plugin:  YouJizz.com, ZoroFiles.com

--- freerapid-plugins/trunk/src/youjizz/cz/vity/freerapid/plugins/services/youjizz/YouJizzFileRunner.java
+++ freerapid-plugins/trunk/src/youjizz/cz/vity/freerapid/plugins/services/youjizz/YouJizzFileRunner.java
@@ -1,4 +1,4 @@
-package cz.vity.freerapid.plugins.services.videomega;
+package cz.vity.freerapid.plugins.services.youjizz;
 
 import cz.vity.freerapid.plugins.exceptions.ErrorDuringDownloadingException;
 import cz.vity.freerapid.plugins.exceptions.ServiceConnectionProblemException;
@@ -6,26 +6,26 @@ import cz.vity.freerapid.plugins.exceptions.URLNotAvailableAnymoreException;
 import cz.vity.freerapid.plugins.webclient.AbstractRunner;
 import cz.vity.freerapid.plugins.webclient.FileState;
 import cz.vity.freerapid.plugins.webclient.utils.PlugUtils;
-import org.apache.commons.httpclient.HttpMethod;
 import org.apache.commons.httpclient.methods.GetMethod;
 
 import java.util.logging.Logger;
+import java.util.regex.Matcher;
 
 /**
  * Class which contains main code
  *
  * @author birchie
  */
-class VideoMegaFileRunner extends AbstractRunner {
-    private final static Logger logger = Logger.getLogger(VideoMegaFileRunner.class.getName());
+class YouJizzFileRunner extends AbstractRunner {
+    private final static Logger logger = Logger.getLogger(YouJizzFileRunner.class.getName());
 
     @Override
-    public void runCheck() throws Exception {
+    public void runCheck() throws Exception { //this method validates file
         super.runCheck();
-        final GetMethod method = getGetMethod(fileURL);
-        if (makeRedirectedRequest(method)) {
+        final GetMethod getMethod = getGetMethod(fileURL);//make first request
+        if (makeRedirectedRequest(getMethod)) {
             checkProblems();
-            checkNameAndSize(getContentAsString());
+            checkNameAndSize(getContentAsString());//ok let's extract file name and size from the page
         } else {
             checkProblems();
             throw new ServiceConnectionProblemException();
@@ -33,8 +33,7 @@ class VideoMegaFileRunner extends AbstractRunner {
     }
 
     private void checkNameAndSize(String content) throws ErrorDuringDownloadingException {
-        PlugUtils.checkName(httpFile, content, ">Videomega.tv - ", "</div>");
-        httpFile.setFileName(httpFile.getFileName() + ".mp4");
+        PlugUtils.checkName(httpFile, content, "<title>", "</title>");
         httpFile.setFileState(FileState.CHECKED_AND_EXISTING);
     }
 
@@ -42,23 +41,17 @@ class VideoMegaFileRunner extends AbstractRunner {
     public void run() throws Exception {
         super.run();
         logger.info("Starting download in TASK " + fileURL);
-        final GetMethod method = getGetMethod(fileURL);
+        final GetMethod method = getGetMethod(fileURL); //create GET request
         if (makeRedirectedRequest(method)) { //we make the main request
             final String contentAsString = getContentAsString();//check for response
             checkProblems();//check problems
-            checkNameAndSize(contentAsString);//extract file name and size from the page
-
-            final String key = PlugUtils.getStringBetween(contentAsString, "hashkey=", "\"");
-            final String size = PlugUtils.getStringBetween(contentAsString, "validateemb.php?", "\"");
-            final String videoPageUrl = "http://videomega.tv/view.php?ref=" + key + "&" + size + "&val=1";
-            if (!makeRedirectedRequest(getMethodBuilder().setReferer(fileURL).setAction(videoPageUrl).toGetMethod())) {
-                checkProblems();
-                throw new ServiceConnectionProblemException();
-            }
-            checkProblems();
-            final String fileUrl = PlugUtils.getStringBetween(getContentAsString(), "<source src=\"", "\"");
-            final HttpMethod httpMethod = getGetMethod(fileUrl);
-            if (!tryDownloadAndSaveFile(httpMethod)) {
+            checkNameAndSize(contentAsString);
+            final String downloadUrl = getMethodBuilder().setReferer(fileURL)
+                    .setActionFromAHrefWhereATagContains("Download").getEscapedURI();
+            final Matcher match = PlugUtils.matcher(".+(\\.\\w+)", downloadUrl);
+            if (match.find())
+                httpFile.setFileName(httpFile.getFileName() + match.group(1));
+            if (!tryDownloadAndSaveFile(getGetMethod(downloadUrl))) {
                 checkProblems();//if downloading failed
                 throw new ServiceConnectionProblemException("Error starting download");//some unknown problem
             }
@@ -70,8 +63,7 @@ class VideoMegaFileRunner extends AbstractRunner {
 
     private void checkProblems() throws ErrorDuringDownloadingException {
         final String contentAsString = getContentAsString();
-        if (contentAsString.contains("<title>404</title>") || contentAsString.contains(">Videomega.tv - </div>") ||
-                contentAsString.contains("the page you were looking for could not be found")) {
+        if (contentAsString.contains("This video does not exist or has been removed")) {
             throw new URLNotAvailableAnymoreException("File not found"); //let to know user in FRD
         }
     }
