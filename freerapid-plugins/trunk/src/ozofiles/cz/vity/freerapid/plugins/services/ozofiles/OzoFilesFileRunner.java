fd3561e67c0c23d935da1d2bc76fb0f944da3ea3
birchie <birchie@40407085-c429-0410-a8d0-eb1a82597f6e>
2015-11-12 22:06:15 +0000

    Fixed:  ClicknUpload.com(+.me), FlashX.tv, TurboBit.net New Plugins:  CloudSix.me, StorBit.net, FilePlaneta.com , OzoFiles.com

---
+++ freerapid-plugins/trunk/src/ozofiles/cz/vity/freerapid/plugins/services/ozofiles/OzoFilesFileRunner.java
@@ -0,0 +1,11 @@
+package cz.vity.freerapid.plugins.services.ozofiles;
+
+import cz.vity.freerapid.plugins.services.xfilesharing.XFileSharingRunner;
+
+/**
+ * Class which contains main code
+ *
+ * @author birchie
+ */
+class OzoFilesFileRunner extends XFileSharingRunner {
+}
--- freerapid-plugins/trunk/src/ozofiles/cz/vity/freerapid/plugins/services/ozofiles/TestApp.java
+++ freerapid-plugins/trunk/src/ozofiles/cz/vity/freerapid/plugins/services/ozofiles/TestApp.java
@@ -1,4 +1,4 @@
-package cz.vity.freerapid.plugins.services.turbobit;
+package cz.vity.freerapid.plugins.services.ozofiles;
 
 import cz.vity.freerapid.plugins.dev.PluginDevApplication;
 import cz.vity.freerapid.plugins.webclient.ConnectionSettings;
@@ -8,7 +8,7 @@ import org.jdesktop.application.Application;
 import java.net.URL;
 
 /**
- * @author Arthur Gunawan
+ * @author birchie
  */
 public class TestApp extends PluginDevApplication {
     @Override
@@ -16,18 +16,18 @@ public class TestApp extends PluginDevApplication {
         final HttpFile httpFile = getHttpFile(); //creates new test instance of HttpFile
         try {
             //we set file URL
-            //httpFile.setNewURL(new URL("http://turbobit.net/j5ymo3novrfi.html"));
-            //    httpFile.setNewURL(new URL("http://turbobit.net/papr4dwbykf9.html"));
-            //httpFile.setNewURL(new URL("http://new.turbobit.net/eu5bhkvpcy7j.html"));
-            //httpFile.setNewURL(new URL("http://turo-bit.net/xfsd1eygf59l/diiig.rar.html"));
-            httpFile.setNewURL(new URL("http://turbobit.net/8e7sqv22luxa.html"));
-            httpFile.setNewURL(new URL("http://dl.rapidlinks.org/urhsrs7kyp50/Poslednee_delo_pirata.2014.SATRip.avi.html"));
-
+            httpFile.setNewURL(new URL("http://ozofiles.com/bworeqqncth0/eyan039.part1.rar.html"));
             //the way we connect to the internet
             final ConnectionSettings connectionSettings = new ConnectionSettings();// creates default connection
             //connectionSettings.setProxy("localhost", 8081); //eg we can use local proxy to sniff HTTP communication
-            //then we tries to download
-            final TurboBitServiceImpl service = new TurboBitServiceImpl(); //instance of service - of our plugin
+            final OzoFilesServiceImpl service = new OzoFilesServiceImpl(); //instance of service - of our plugin
+            /*
+            //we set premium account details
+            final PremiumAccount config = new PremiumAccount();
+            config.setUsername("****");
+            config.setPassword("****");
+            service.setConfig(config);
+            //*/
             //runcheck makes the validation
             testRun(service, httpFile, connectionSettings);//download file with service and its Runner
             //all output goes to the console
