9a8e27f82c0b91242a894a2b656f617bce283302
tong2shot <tong2shot@40407085-c429-0410-a8d0-eb1a82597f6e>
2016-02-14 22:02:34 +0000

    - Add support for connection settings (application, direct, custom local proxy) per download file. - DownloadFileModel schema changes: add localConnectionSettingsType, localProxy.

--- freerapid/branches/tong2shot/src/cz/vity/freerapid/gui/content/ContentPanel.java
+++ freerapid/branches/tong2shot/src/cz/vity/freerapid/gui/content/ContentPanel.java
@@ -13,6 +13,7 @@ import cz.vity.freerapid.gui.managers.DataManager;
 import cz.vity.freerapid.gui.managers.ManagerDirector;
 import cz.vity.freerapid.gui.managers.MenuManager;
 import cz.vity.freerapid.gui.managers.exceptions.NotSupportedDownloadServiceException;
+import cz.vity.freerapid.model.LocalConnectionSettingsType;
 import cz.vity.freerapid.model.bean.DownloadFile;
 import cz.vity.freerapid.plugins.webclient.ConnectionSettings;
 import cz.vity.freerapid.plugins.webclient.DownloadState;
@@ -1316,7 +1317,21 @@ public class ContentPanel extends JPanel implements ListSelectionListener, ListD
 
         boolean forceEnabled = isSelectedEnabled() && this.manager.hasDownloadFilesStates(selectedRows, DownloadsActions.forceEnabledStates);
         forceMenu.setEnabled(forceEnabled);
-        final List<ConnectionSettings> connectionSettingses = director.getClientManager().getAvailableConnections();
+        final List<ConnectionSettings> connectionSettingses = new ArrayList<ConnectionSettings>();
+        connectionSettingses.addAll(director.getClientManager().getAvailableConnections());
+        for (DownloadFile file : files) {
+            if (file.getLocalConnectionSettingsType() == LocalConnectionSettingsType.DIRECT) {
+                ConnectionSettings direct = new ConnectionSettings();
+                if (!connectionSettingses.contains(direct)) {
+                    connectionSettingses.add(direct);
+                }
+            } else if (file.getLocalConnectionSettingsType() == LocalConnectionSettingsType.LOCAL_PROXY) {
+                ConnectionSettings proxy = director.getClientManager().getProxyConnection(file.getLocalProxy(), false);
+                if (!connectionSettingses.contains(proxy)) {
+                    connectionSettingses.add(proxy);
+                }
+            }
+        }
         boolean anyEnabled = false;
         for (ConnectionSettings settings : connectionSettingses) {
             final ForceDownloadAction action = new ForceDownloadAction(settings);
---
+++ freerapid/branches/tong2shot/src/cz/vity/freerapid/gui/dialogs/LocalConnectionSettingsDialog.java
@@ -0,0 +1,242 @@
+package cz.vity.freerapid.gui.dialogs;
+
+import com.jgoodies.binding.PresentationModel;
+import com.jgoodies.binding.adapter.Bindings;
+import com.jgoodies.forms.builder.PanelBuilder;
+import com.jgoodies.forms.factories.Borders;
+import com.jgoodies.forms.layout.*;
+import cz.vity.freerapid.gui.managers.ManagerDirector;
+import cz.vity.freerapid.model.LocalConnectionSettingsType;
+import cz.vity.freerapid.model.bean.DownloadFile;
+import cz.vity.freerapid.swing.Swinger;
+import cz.vity.freerapid.utilities.LogUtils;
+import org.jdesktop.swinghelper.buttonpanel.JXButtonPanel;
+
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.logging.Logger;
+
+/**
+ * @author tong2shot
+ */
+public class LocalConnectionSettingsDialog extends AppFrame {
+
+    private final static Logger logger = Logger.getLogger(LocalConnectionSettingsDialog.class.getName());
+
+    private final static String PROXY_REGEX_PATTERN = "^(?i)(\\$SOCKS\\$|SOCKS\\:)((\\w*)(:(.*?))?@)?(.*?):(\\d{2,5})";
+
+    private final ManagerDirector director;
+    private final DownloadFile file;
+    private PresentationModel<DownloadFile> model;
+
+    public LocalConnectionSettingsDialog(Frame owner, ManagerDirector director, DownloadFile file) {
+        super(owner);
+        this.director = director;
+        this.file = file;
+        this.setName("localConnectionSettingsDialog");
+        try {
+            initComponents();
+            build();
+        } catch (Exception e) {
+            LogUtils.processException(logger, e);
+            doClose();
+        }
+    }
+
+    @Override
+    protected AbstractButton getBtnCancel() {
+        return cancelButton;
+    }
+
+    @Override
+    protected AbstractButton getBtnOK() {
+        return okButton;
+    }
+
+    @org.jdesktop.application.Action
+    public void cancelBtnAction() {
+        setResult(RESULT_CANCEL);
+        if (model != null)
+            model.triggerFlush();
+        doClose();
+    }
+
+    @org.jdesktop.application.Action
+    public void okBtnAction() {
+        if (!validateChanges())
+            return;
+        setResult(RESULT_OK);
+        if (model != null)
+            model.triggerCommit();
+        doClose();
+    }
+
+    @Override
+    public void doClose() {
+        super.doClose();
+        if (model != null) {
+            model.setBean(null);
+        }
+    }
+
+    private void build() {
+        inject();
+        buildModels();
+        buildGUI();
+        setAction(okButton, "okBtnAction");
+        setAction(cancelButton, "cancelBtnAction");
+    }
+
+    private void buildModels() {
+        model = new PresentationModel<DownloadFile>(file);
+        Bindings.bind(rbApplication, model.getBufferedModel("localConnectionSettingsType"), LocalConnectionSettingsType.APPLICATION);
+        Bindings.bind(rbDirect, model.getBufferedModel("localConnectionSettingsType"), LocalConnectionSettingsType.DIRECT);
+        Bindings.bind(rbLocalProxy, model.getBufferedModel("localConnectionSettingsType"), LocalConnectionSettingsType.LOCAL_PROXY);
+        Bindings.bind(fldLocalProxy, model.getBufferedModel("localProxy"));
+    }
+
+    private void buildGUI() {
+        rbApplication.setActionCommand(rbApplication.getName());
+        rbDirect.setActionCommand(rbDirect.getName());
+        rbLocalProxy.setActionCommand(rbLocalProxy.getName());
+        ActionListener rbActionListener = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                boolean isLocalProxy = e.getActionCommand().equals(rbLocalProxy.getActionCommand());
+                fldLocalProxy.setEnabled(isLocalProxy);
+                fldLocalProxy.setEditable(isLocalProxy);
+            }
+        };
+        rbApplication.addActionListener(rbActionListener);
+        rbDirect.addActionListener(rbActionListener);
+        rbLocalProxy.addActionListener(rbActionListener);
+
+        boolean isLocalProxy = rbLocalProxy.isSelected();
+        fldLocalProxy.setEnabled(isLocalProxy);
+        fldLocalProxy.setEditable(isLocalProxy);
+
+        //fldLocalProxy.setDocument(new LimitedPlainDocument(PROXY_REGEX_PATTERN));
+    }
+
+
+    private boolean validateChanges() {
+        if (rbLocalProxy.isSelected() && !fldLocalProxy.getText().matches(PROXY_REGEX_PATTERN)) {
+            Swinger.showErrorMessage(this.getResourceMap(), "invalidProxyMessage");
+            Swinger.inputFocus(fldLocalProxy);
+            fldLocalProxy.selectAll();
+            return false;
+        }
+        return true;
+    }
+
+    @SuppressWarnings({"deprecation"})
+    private void initComponents() {
+        JPanel dialogPane = new JPanel();
+        JPanel contentPanel = new JPanel();
+
+        rbApplication = new JRadioButton();
+        rbDirect = new JRadioButton();
+        rbLocalProxy = new JRadioButton();
+        fldLocalProxy = new JTextField();
+        ButtonGroup buttonGroup = new ButtonGroup();
+        buttonGroup.add(rbApplication);
+        buttonGroup.add(rbDirect);
+        buttonGroup.add(rbLocalProxy);
+        JLabel lblProxyDesc = new JLabel();
+
+        JXButtonPanel buttonBar = new JXButtonPanel();
+        okButton = new JButton();
+        cancelButton = new JButton();
+        CellConstraints cc = new CellConstraints();
+
+        //======== this ========
+        Container contentPane = getContentPane();
+        contentPane.setLayout(new BorderLayout());
+
+        //======== dialogPane ========
+        {
+            dialogPane.setBorder(Borders.DIALOG);
+            dialogPane.setLayout(new BorderLayout());
+
+            //======== contentPanel ========
+            {
+                //---- rbApplication ----
+                rbApplication.setName("rbApplication");
+
+                //---- rbDirect ----
+                rbDirect.setName("rbDirect");
+
+                //---- rbLocalProxy ----
+                rbLocalProxy.setName("rbLocalProxy");
+
+                //---- fldLocalProxy ----
+                fldLocalProxy.setName("fldLocalProxy");
+
+                //---- lblProxyDesc ----
+                lblProxyDesc.setName("lblProxyDesc");
+
+                PanelBuilder contentPanelBuilder = new PanelBuilder(new FormLayout(
+                        new ColumnSpec[]{
+                                FormSpecs.DEFAULT_COLSPEC,
+                        },
+                        new RowSpec[]{
+                                FormSpecs.DEFAULT_ROWSPEC,
+                                FormSpecs.LINE_GAP_ROWSPEC,
+                                FormSpecs.DEFAULT_ROWSPEC,
+                                FormSpecs.LINE_GAP_ROWSPEC,
+                                FormSpecs.DEFAULT_ROWSPEC,
+                                FormSpecs.LINE_GAP_ROWSPEC,
+                                FormSpecs.DEFAULT_ROWSPEC,
+                                FormSpecs.LINE_GAP_ROWSPEC,
+                                FormSpecs.DEFAULT_ROWSPEC,
+                                FormSpecs.LINE_GAP_ROWSPEC,
+                                FormSpecs.DEFAULT_ROWSPEC
+                        }), contentPanel);
+
+                contentPanelBuilder.add(rbApplication, cc.xy(1, 1));
+                contentPanelBuilder.add(rbDirect, cc.xy(1, 3));
+                contentPanelBuilder.add(rbLocalProxy, cc.xy(1, 5));
+                contentPanelBuilder.add(fldLocalProxy, cc.xy(1, 7));
+                contentPanelBuilder.add(lblProxyDesc, cc.xy(1, 9));
+            }
+            dialogPane.add(contentPanel, BorderLayout.CENTER);
+
+            //======== buttonBar ========
+            {
+                buttonBar.setBorder(new EmptyBorder(12, 0, 0, 0));
+
+                //---- okButton ----
+                okButton.setName("okButton");
+
+                //---- cancelButton ----
+                cancelButton.setName("cancelButton");
+
+                PanelBuilder buttonBarBuilder = new PanelBuilder(new FormLayout(
+                        new ColumnSpec[]{
+                                new ColumnSpec(ColumnSpec.FILL, Sizes.DEFAULT, FormSpec.DEFAULT_GROW),
+                                FormSpecs.UNRELATED_GAP_COLSPEC,
+                                ColumnSpec.decode("max(pref;42dlu)"),
+                                FormSpecs.LABEL_COMPONENT_GAP_COLSPEC,
+                                FormSpecs.DEFAULT_COLSPEC
+                        },
+                        RowSpec.decodeSpecs("fill:pref")), buttonBar);
+                ((FormLayout) buttonBar.getLayout()).setColumnGroups(new int[][]{{3, 5}});
+
+                buttonBarBuilder.add(okButton, cc.xy(3, 1));
+                buttonBarBuilder.add(cancelButton, cc.xy(5, 1));
+            }
+            dialogPane.add(buttonBar, BorderLayout.SOUTH);
+        }
+        contentPane.add(dialogPane, BorderLayout.CENTER);
+    }
+
+    private JRadioButton rbApplication;
+    private JRadioButton rbDirect;
+    private JRadioButton rbLocalProxy;
+    private JTextField fldLocalProxy;
+    private JButton okButton;
+    private JButton cancelButton;
+}
