b9e22937dface5b1c945e39e664110da8cb5f187
tong2shot <tong2shot@40407085-c429-0410-a8d0-eb1a82597f6e>
2016-04-04 12:35:48 +0000

    Vimeo v1.5.0: support for original video.

--- freerapid-plugins/trunk/src/vimeo/cz/vity/freerapid/plugins/services/vimeo/VideoQuality.java
+++ freerapid-plugins/trunk/src/vimeo/cz/vity/freerapid/plugins/services/vimeo/VideoQuality.java
@@ -10,12 +10,21 @@ enum VideoQuality {
     Mobile(270),
     SD(360),
     HD(720),
-    HD1080(1080);
+    HD1080(1080),
+    Original(10000, "Original");
+
 
     private final int quality;
+    private final String name;
 
     VideoQuality(int quality) {
         this.quality = quality;
+        this.name = name() + " (" + quality + "p)";
+    }
+
+    VideoQuality(int quality, String name) {
+        this.quality = quality;
+        this.name = name;
     }
 
     public int getQuality() {
@@ -24,7 +33,7 @@ enum VideoQuality {
 
     @Override
     public String toString() {
-        return name() + " (" + quality + "p)";
+        return name;
     }
 
     public static VideoQuality[] getItems() {
