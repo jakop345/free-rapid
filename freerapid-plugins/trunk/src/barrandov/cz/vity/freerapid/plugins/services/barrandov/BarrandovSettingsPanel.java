aabea1c96157a82ae6a6b8e747aefbe78df27dd9
birchie <birchie@40407085-c429-0410-a8d0-eb1a82597f6e>
2016-05-04 07:28:04 +0000

    Fixed:  Barrandov.tv, Tumblr.com

--- freerapid-plugins/trunk/src/barrandov/cz/vity/freerapid/plugins/services/barrandov/BarrandovSettingsPanel.java
+++ freerapid-plugins/trunk/src/barrandov/cz/vity/freerapid/plugins/services/barrandov/BarrandovSettingsPanel.java
@@ -10,9 +10,11 @@ import java.awt.event.ActionListener;
  */
 public class BarrandovSettingsPanel extends JPanel {
     private BarrandovSettingsConfig config;
+    private BarrandovServiceImpl service;
 
     public BarrandovSettingsPanel(BarrandovServiceImpl service) throws Exception {
         super();
+        this.service = service;
         config = service.getConfig();
         initPanel();
     }
@@ -22,8 +24,10 @@ public class BarrandovSettingsPanel extends JPanel {
 
         final JLabel qualityLabel = new JLabel("Preferred quality level:");
         final JComboBox qualityList = new JComboBox(qualityStrings);
+        final JButton accountButton = new JButton("Premium Account");
         qualityLabel.setAlignmentX(Component.LEFT_ALIGNMENT);
         qualityList.setAlignmentX(Component.LEFT_ALIGNMENT);
+        accountButton.setAlignmentX(Component.LEFT_ALIGNMENT);
         int qs = config.getQualitySetting();
         qualityList.setSelectedIndex(qs);
         qualityList.addActionListener(new ActionListener() {
@@ -32,9 +36,17 @@ public class BarrandovSettingsPanel extends JPanel {
                 config.setQualitySetting(qualityList.getSelectedIndex());
             }
         });
+        accountButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                service.showAccount();
+            }
+        });
         this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
         add(qualityLabel);
         add(qualityList);
+        add(new JLabel(" "));
+        add(accountButton);
         //add(Box.createRigidArea(new Dimension(0, 15)));
         setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
     }
